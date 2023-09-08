afdd56bcde233edddaf4e240604cf56936bd58e0
Dipali Mehta <dipalivmehta016@gmail.com>
2023-09-08 15:09:24 +0530

    commit

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-from datetime import datetime, timedelta
-from random import randint
 import os
+from random import randint
+from datetime import datetime, timedelta
+
+repo_url = "https://github.com/dipalimehta/hello_world.git"
+local_repo_path = r"D:\STUDY MATERIAL\1.Life_Changing_folder\hello_world"  # Replace with the desired local directory
+
+def run_git_command(command):
+    print(f"Executing: {command}")
+    os.system(command)
+
+# Clone the repository
+run_git_command(f'git clone {repo_url} {local_repo_path}')
 
 for i in range(100, 365):
     for j in range(0, randint(1, 10)):
         days_ago = timedelta(days=i)
         date_string = (datetime.now() - days_ago).strftime('%Y-%m-%d')
 
-        with open('file.txt', 'a') as file:
+        with open(os.path.join(local_repo_path, 'file.txt'), 'a') as file:
             file.write(date_string + '\n')
 
-        os.system('git add .')
-        os.system('git commit --date="' + date_string + '" -m "commit"')
+        run_git_command(f'cd {local_repo_path} && git add .')
+        run_git_command(f'cd {local_repo_path} && git commit --date="{date_string}" -m "commit"')
 
-os.system('git push -u origin main')
+run_git_command(f'cd {local_repo_path} && git push -u origin main')
+
+
+
+
+
+
+# from datetime import datetime, timedelta
+# from random import randint
+# import os
+
+# for i in range(100, 365):
+#     for j in range(0, randint(1, 10)):
+#         days_ago = timedelta(days=i)
+#         date_string = (datetime.now() - days_ago).strftime('%Y-%m-%d')
+
+#         with open('file.txt', 'a') as file:
+#             file.write(date_string + '\n')
+
+#         os.system('git add .')
+#         os.system('git commit --date="' + date_string + '" -m "commit"')
+
+# os.system('git push -u origin main')
 
 
 # import os
