fe498616c3d5bc012d54cb5fa44cd76c9683a1d6
Dipali Mehta <dipalivmehta016@gmail.com>
2023-12-14 21:52:56 +0530

    commit

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
+# import os
+# from random import randint
+
+# for i in range(1,365):
+#     for j in range(0,randint(1,10)):
+#         d = str(i) + 'days ago'
+#         with open('file.txt','a') as file:
+#             file.write(d)
+#         os.system('git add .')
+#         os.system('git commit --date"' + d + '" -m "commit"')
+# os.system('git push -u origin main')
+
 import os
 from random import randint
+from datetime import datetime, timedelta
 
-for i in range(1,365):
-    for j in range(0,randint(1,10)):
-        d = str(i) + 'days ago'
-        with open('file.txt','a') as file:
-            file.write(d)
+for i in range(1, 365):
+    for j in range(0, randint(1, 10)):
+        days_ago = timedelta(days=i)
+        commit_date = (datetime.now() - days_ago).strftime('%Y-%m-%d %H:%M:%S')
+        
+        with open('file.txt', 'a') as file:
+            file.write(str(i) + ' days ago\n')
+        
         os.system('git add .')
-        os.system('git commit --date"' + d + '" -m "commit"')
+        os.system(f'git commit --date="{commit_date}" -m "commit"')
+
 os.system('git push -u origin main')
